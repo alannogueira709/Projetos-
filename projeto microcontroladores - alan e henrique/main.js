ad5c2383ea04dd22cb4c8b964a58cc2dfb7c92c7
Alan Dionisio <108749159+alannogueira709@users.noreply.github.com>
2023-07-13 17:13:27 -0300

    Add files via upload

---
+++ projeto microcontroladores - alan e henrique/main.js
@@ -0,0 +1,9 @@
+
+// Path: index.html
+const botaoCima = document.getElementsByClassName("cima");
+const botaoBaixo = document.getElementsByClassName("baixo");
+const botaoEsquerda = document.getElementsByClassName("esquerda");
+const botaoDireita = document.getElementsByClassName("direita");
+const garraCima = document.getElementsByClassName("garraCima");
+const garraBaixo = document.getElementsByClassName("garraBaixo");
+
